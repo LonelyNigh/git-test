c5a81c8d1d505133b3e44f5af748bc1aadee3854
CY <CY@night.com>
2023-02-08 12:05:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+
+        System.out.print("Hello.git!");
+        System.out.print("Hello.git2!");
+    }
+}
